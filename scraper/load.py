53698dcf1dd0adabaa1d1a44ecc12d5b660cbf5b
James Shanley <js10587@hotmail.com>
2026-01-21 21:21:56 +0000

    added logging to the build

--- scraper/load.py
+++ scraper/load.py
@@ -1,9 +1,15 @@
 import json
+import logging
 from pathlib import Path
 
+logger = logging.getLogger(__name__)
 
 def save_to_json(hierarchy, path: str):
+    logger.info(f"Saving hierarchy to {path}")
+
     Path(path).parent.mkdir(parents=True, exist_ok=True)
 
     with open(path, "w", encoding="utf-8") as f:
         json.dump(hierarchy, f, ensure_ascii=False, indent=2)
+
+    logger.info("Save complete")
--- scraper/main.py
+++ scraper/main.py
@@ -1,19 +1,21 @@
+from scraper.config import load_config
 from scraper.browser import create_driver
 from scraper.extract import extract_headings
 from scraper.transform import build_heading_hierarchy, print_hierarchy
 from scraper.load import save_to_json
+from scraper.logging_setup import setup_logging
 
+def run():
+    config = load_config()
+    setup_logging(config["logging"]["level"])
 
-def run(url: str, output_path: str):
     driver = create_driver()
-    headings = extract_headings(driver, url)
+    headings = extract_headings(driver, config)
     driver.quit()
 
     hierarchy = build_heading_hierarchy(headings)
 
-    save_to_json(hierarchy, output_path)
-    print(f"\nFile saved to:\n {output_path}\n")
-
+    save_to_json(hierarchy, config["output"]["path"])
 
 if __name__ == "__main__":
-    run("https://www.bbc.co.uk", "data/headings.json")
+    run()
